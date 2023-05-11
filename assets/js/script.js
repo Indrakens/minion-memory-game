065b523759ec8cfadcb39a4c3dbe73714271c012
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-05-11 12:53:09 +0000

    Add text link to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
 let section = document.querySelector("section");
 let playerLivesCount = document.querySelector("span");
 let playerLives = 8;
+
+//Text link
+playerLivesCount.textContent = playerLives;
