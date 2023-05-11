0b000e54445d9b46cc550fb300b51fdd6bb7a440
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-05-11 13:00:54 +0000

    Add images to image data in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,27 @@ let playerLives = 8;
 
 //Text link
 playerLivesCount.textContent = playerLives;
+
+//Image data
+let getData = () => [
+    {imgSrc: "./assets/images/minion1.png", name: "minion1"},
+    {imgSrc: "./assets/images/minion2.png", name: "minion2"},
+    {imgSrc: "./assets/images/minion3.png", name: "minion3"},
+    {imgSrc: "./assets/images/minion4.png", name: "minion4"},
+    {imgSrc: "./assets/images/minion5.png", name: "minion5"},
+    {imgSrc: "./assets/images/minion6.png", name: "minion6"},
+    {imgSrc: "./assets/images/minion7.png", name: "minion7"},
+    {imgSrc: "./assets/images/minion8.png", name: "minion8"},
+    {imgSrc: "./assets/images/minion9.png", name: "minion9"},
+    {imgSrc: "./assets/images/minion10.png", name: "minion10"},
+    {imgSrc: "./assets/images/minion1.png", name: "minion1"},
+    {imgSrc: "./assets/images/minion2.png", name: "minion2"},
+    {imgSrc: "./assets/images/minion3.png", name: "minion3"},
+    {imgSrc: "./assets/images/minion4.png", name: "minion4"},
+    {imgSrc: "./assets/images/minion5.png", name: "minion5"},
+    {imgSrc: "./assets/images/minion6.png", name: "minion6"},
+    {imgSrc: "./assets/images/minion7.png", name: "minion7"},
+    {imgSrc: "./assets/images/minion8.png", name: "minion8"},
+    {imgSrc: "./assets/images/minion9.png", name: "minion9"},
+    {imgSrc: "./assets/images/minion10.png", name: "minion10"},
+]
