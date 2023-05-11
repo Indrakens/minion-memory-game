44636d3e50621147ff7177e84c53007ef4ffe237
Indrakens Zilinska <indrazilinska83@gmail.com>
2023-05-11 22:05:36 +0000

    Add game rules page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,60 +6,119 @@ let playerLives = 8;
 playerLivesCount.textContent = playerLives;
 
 //Image data
-let getData = () => [
-    {imgSrc: "./assets/images/minion1.png", name: "minion1"},
-    {imgSrc: "./assets/images/minion2.png", name: "minion2"},
-    {imgSrc: "./assets/images/minion3.png", name: "minion3"},
-    {imgSrc: "./assets/images/minion4.png", name: "minion4"},
-    {imgSrc: "./assets/images/minion5.png", name: "minion5"},
-    {imgSrc: "./assets/images/minion6.png", name: "minion6"},
-    {imgSrc: "./assets/images/minion7.png", name: "minion7"},
-    {imgSrc: "./assets/images/minion8.png", name: "minion8"},
-    {imgSrc: "./assets/images/minion9.png", name: "minion9"},
-    {imgSrc: "./assets/images/minion10.png", name: "minion10"},
-    {imgSrc: "./assets/images/minion1.png", name: "minion1"},
-    {imgSrc: "./assets/images/minion2.png", name: "minion2"},
-    {imgSrc: "./assets/images/minion3.png", name: "minion3"},
-    {imgSrc: "./assets/images/minion4.png", name: "minion4"},
-    {imgSrc: "./assets/images/minion5.png", name: "minion5"},
-    {imgSrc: "./assets/images/minion6.png", name: "minion6"},
-    {imgSrc: "./assets/images/minion7.png", name: "minion7"},
-    {imgSrc: "./assets/images/minion8.png", name: "minion8"},
-    {imgSrc: "./assets/images/minion9.png", name: "minion9"},
-    {imgSrc: "./assets/images/minion10.png", name: "minion10"},
+let getData = () => [{
+        imgSrc: "./assets/images/minion1.png",
+        name: "minion1"
+    },
+    {
+        imgSrc: "./assets/images/minion2.png",
+        name: "minion2"
+    },
+    {
+        imgSrc: "./assets/images/minion3.png",
+        name: "minion3"
+    },
+    {
+        imgSrc: "./assets/images/minion4.png",
+        name: "minion4"
+    },
+    {
+        imgSrc: "./assets/images/minion5.png",
+        name: "minion5"
+    },
+    {
+        imgSrc: "./assets/images/minion6.png",
+        name: "minion6"
+    },
+    {
+        imgSrc: "./assets/images/minion7.png",
+        name: "minion7"
+    },
+    {
+        imgSrc: "./assets/images/minion8.png",
+        name: "minion8"
+    },
+    {
+        imgSrc: "./assets/images/minion9.png",
+        name: "minion9"
+    },
+    {
+        imgSrc: "./assets/images/minion10.png",
+        name: "minion10"
+    },
+    {
+        imgSrc: "./assets/images/minion1.png",
+        name: "minion1"
+    },
+    {
+        imgSrc: "./assets/images/minion2.png",
+        name: "minion2"
+    },
+    {
+        imgSrc: "./assets/images/minion3.png",
+        name: "minion3"
+    },
+    {
+        imgSrc: "./assets/images/minion4.png",
+        name: "minion4"
+    },
+    {
+        imgSrc: "./assets/images/minion5.png",
+        name: "minion5"
+    },
+    {
+        imgSrc: "./assets/images/minion6.png",
+        name: "minion6"
+    },
+    {
+        imgSrc: "./assets/images/minion7.png",
+        name: "minion7"
+    },
+    {
+        imgSrc: "./assets/images/minion8.png",
+        name: "minion8"
+    },
+    {
+        imgSrc: "./assets/images/minion9.png",
+        name: "minion9"
+    },
+    {
+        imgSrc: "./assets/images/minion10.png",
+        name: "minion10"
+    },
 ]
 
 //Randomize function
 let randomize = () => {
     let cardData = getData();
-    cardData.sort( () => Math.random() -0.5);
+    cardData.sort(() => Math.random() - 0.5);
     return cardData;
 };
 
 //Card function
 let cardGenerator = () => {
-  let cardData = randomize(); 
- 
-cardData.forEach((item) => {
-    let card = document.createElement("div");
-    let face = document.createElement("img");
-    let back = document.createElement("div");
-    card.classList = "card";
-    face.classList = "face";
-    back.classList = "back";
-    //Attach images for cards
-    face.src = item.imgSrc;
-    card.setAttribute("name", item.name);
-    //Attached cards to section
-    section.appendChild(card);
-    card.appendChild(face);
-    card.appendChild(back); 
+    let cardData = randomize();
 
-    card.addEventListener("click", (e) =>  {
-        card.classList.toggle("toggleCard");
-        checkCards(e);
+    cardData.forEach((item) => {
+        let card = document.createElement("div");
+        let face = document.createElement("img");
+        let back = document.createElement("div");
+        card.classList = "card";
+        face.classList = "face";
+        back.classList = "back";
+        //Attach images for cards
+        face.src = item.imgSrc;
+        card.setAttribute("name", item.name);
+        //Attached cards to section
+        section.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+
+        card.addEventListener("click", (e) => {
+            card.classList.toggle("toggleCard");
+            checkCards(e);
+        });
     });
-});
 };
 
 //Card check
@@ -70,30 +129,30 @@ let checkCards = (e) => {
     let flippedCards = document.querySelectorAll(".flipped");
     let toggleCard = document.querySelectorAll(".toggleCard");
     console.log(flippedCards);
-    
-    if (flippedCards.length === 2) {
-       if (flippedCards[0].getAttribute("name") === 
-       flippedCards[1].getAttribute("name")
-       ) {
-        console.log("match");
-        flippedCards.forEach((card) => {
-            card.classList.remove("flipped");
-            card.style.pointerEvents = "none";
-        });
-       } else {
-        console.log("wrong");
-        flippedCards.forEach((card) => {
-            card.classList.remove("flipped");
-            setTimeout(() => card.classList.remove("toggleCard"), 1000);
-        });
-        playerLives--;
-        playerLivesCount.textContent = playerLives;
 
-        //Alert window if loose the game
-        if (playerLives === 0){
-            restart("Oops! Try again!");
-        };
-       }
+    if (flippedCards.length === 2) {
+        if (flippedCards[0].getAttribute("name") ===
+            flippedCards[1].getAttribute("name")
+        ) {
+            console.log("match");
+            flippedCards.forEach((card) => {
+                card.classList.remove("flipped");
+                card.style.pointerEvents = "none";
+            });
+        } else {
+            console.log("wrong");
+            flippedCards.forEach((card) => {
+                card.classList.remove("flipped");
+                setTimeout(() => card.classList.remove("toggleCard"), 1000);
+            });
+            playerLives--;
+            playerLivesCount.textContent = playerLives;
+
+            //Alert window if loose the game
+            if (playerLives === 0) {
+                restart("Oops! Try again!");
+            };
+        }
     }
 
     //Alert window if won the game
@@ -104,24 +163,24 @@ let checkCards = (e) => {
 
 //Restart game
 let restart = (text) => {
-   let cardData = randomize();
-   let faces = document.querySelectorAll(".face");
-   let cards = document.querySelectorAll(".card");
-   section.style.pointerEvents = "none";
-   cardData.forEach((item, index) => {
-    cards[index].classList.remove("toggleCard");
-  
-    //Randomize cards
-    setTimeout(() => {
-    cards[index].style.pointerEvents = "all";
-    faces[index].src = item.imgSrc;
-    cards[index].setAttribute("name", item.name);
-    section.style.pointerEvents = "all";
-    }, 1000);
-   });
-   playerLives = 8;
-   playerLivesCount.textContent = playerLives;
-   setTimeout(() => window.alert(text), 120);
+    let cardData = randomize();
+    let faces = document.querySelectorAll(".face");
+    let cards = document.querySelectorAll(".card");
+    section.style.pointerEvents = "none";
+    cardData.forEach((item, index) => {
+        cards[index].classList.remove("toggleCard");
+
+        //Randomize cards
+        setTimeout(() => {
+            cards[index].style.pointerEvents = "all";
+            faces[index].src = item.imgSrc;
+            cards[index].setAttribute("name", item.name);
+            section.style.pointerEvents = "all";
+        }, 1000);
+    });
+    playerLives = 8;
+    playerLivesCount.textContent = playerLives;
+    setTimeout(() => window.alert(text), 120);
 };
 
 
